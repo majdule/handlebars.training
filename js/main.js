febaa8dd5199c9e0dec9450da2ed211361ad0767
MAJ1BG <dusan.majstorovic@bosch.com>
2018-02-27 15:24:55 +0100

    Move data from js to json

--- js/main.js
+++ js/main.js
@@ -1,55 +1,11 @@
-var cast = {
-  "characters": [
-    {
-      "name": "Jon Snow",
-      "shortCode": "jon-snow",
-      "actor": "Kit Harrington",
-      "house": "Stark",
-      "location": "The Wall"
-    },
-    {
-      "name": "Tyrion Lannister",
-      "shortCode": "tyrion",
-      "actor": "Peter Dinklage",
-      "house": "Lannister",
-      "location": "Dragonstone"
-    },
-    {
-      "name": "Brienne of Tarth",
-      "shortCode": "brienne",
-      "actor": "Gwendoline Christie",
-      "house": "Stark",
-      "location": "Winterfell"
-    },
-    {
-      "name": "Eddard Stark",
-      "shortCode": "ned-stark",
-      "actor": "Sean Bean",
-      "house": "Stark",
-      "location": ""
-    },
-    {
-      "name": "Sandor Clegane",
-      "shortCode": "the-hound",
-      "actor": "Rory McCann",
-      "house": "Clegane",
-      "location": ""
-    },
-    {
-      "name": "Daenerys Targaryen",
-      "shortCode": "dany",
-      "actor": "Emilia Clarke",
-      "house": "Targaryen",
-      "location": "Mereen"
-    }
-  ]
-}
-
 $(document).ready(function () {
   var characterTemplate = $("#character-template").html();
 
   var compiledCharacterTemplate = Handlebars.compile(characterTemplate);
 
-  $(".character-list-container").html(compiledCharacterTemplate(cast));
+
+  $.ajax("./data/cast.json").done(function (cast) {
+    $(".character-list-container").html(compiledCharacterTemplate(cast));
+  });
 
 });
