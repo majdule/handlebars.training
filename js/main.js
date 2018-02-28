b16170f0deeee2e85bb81e0ed5a7d0de77642462
MAJ1BG <dusan.majstorovic@bosch.com>
2018-02-28 09:56:05 +0100

    Custom helpers

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+Handlebars.registerHelper("formatName", function(property1, property2){
+  return new Handlebars.SafeString("Hello my name is <strong>" + property1 + "</strong> and I live at <strong>" + property2 + "</strong>");
+});
+
+Handlebars.registerHelper("formatPhoneNumber", function(property) {
+  if(property) {
+  var phone = property.toString();
+  return "(" + phone.substr(0, 3) + ") " + phone.substr(3, 3) + "-" + phone.substr(6, 4);
+  }
+});
+
 $(document).ready(function () {
   var characterTemplate = $("#character-template").html();
 
